01f70cb81ed439fc32acdaeb1bbcf04050b15d18
amirb <amir.bullock9@gmail.com>
2023-04-12 23:35:32 -0400

    add pokemon data

---
+++ data/pokemon-data.js
@@ -0,0 +1,15 @@
+const pokemon = [
+  {name: 'Tyranitar', starter: false, _id: 248},
+  {name: 'Pupitar', starter: false, _id: 247},
+  {name: 'Larvitar', starter: false, _id: 246},
+  {name: 'Pikachu', starter: true, _id: 025},
+  {name: 'Charmander', starter: false, _id: 004},
+  {name: 'Bulbasaur', starter: false, _id: 001},
+  {name: 'Squirtle', starter: false, _id: 007},
+  {name: 'Wooper', starter: false, _id: 194},
+  {name: 'Quagsire', starter: false, _id: 195}
+]
+
+export {
+  pokemon
+}
