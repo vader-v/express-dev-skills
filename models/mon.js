6e2d7e5337eaf218a440dcae889a7281eb94327a
amirb <amir.bullock9@gmail.com>
2023-04-14 08:39:44 -0400

    add mongoose import to monSchema and export model

---
+++ models/mon.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema
+
+const monSchema = new Schema({
+  name: String,
+  starter: Boolean,
+})
+
+//Compile schema into model and export it
+const Mon = mongoose.model('Mon', monSchema)
+
+export {
+  Mon
+}
