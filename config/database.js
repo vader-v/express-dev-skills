d0314ba7aec4afc440cb9335fa327fb3a081863a
amirb <amir.bullock9@gmail.com>
2023-04-14 08:27:47 -0400

    add mongoose connection shortcut

--- config/database.js
+++ config/database.js
@@ -1,4 +1,11 @@
 import mongoose from 'mongoose'
 
+// shortcut to mongoose.connection obj
+const db = mongoose.connection
+
 // connect to the database
 mongoose.connect(process.env.DATABASE_URL)
+
+db.on('connected', function(){
+  console.log(`Connected to Mongodb ${db.name} at ${db.host}:${db.port}`)
+})
