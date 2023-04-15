b84ff30c377d4612f1755b373ffdd65668631e6c
amirb <amir.bullock9@gmail.com>
2023-04-15 15:04:02 -0400

    update label names for forms and data values

--- data/pokemon-data.js
+++ data/pokemon-data.js
@@ -3,9 +3,9 @@ const pokemon = [
   {name: 'Pupitar', starter: false, _id: 111247},
   {name: 'Larvitar', starter: false, _id: 111246},
   {name: 'Pikachu', starter: true, _id: 111025},
-  {name: 'Charmander', starter: false, _id: 111004},
-  {name: 'Bulbasaur', starter: false, _id: 111001},
-  {name: 'Squirtle', starter: false, _id: 111007},
+  {name: 'Charmander', starter: true, _id: 111004},
+  {name: 'Bulbasaur', starter: true, _id: 111001},
+  {name: 'Squirtle', starter: true, _id: 111007},
   {name: 'Wooper', starter: false, _id: 111194},
   {name: 'Quagsire', starter: false, _id: 111195},
 ]
